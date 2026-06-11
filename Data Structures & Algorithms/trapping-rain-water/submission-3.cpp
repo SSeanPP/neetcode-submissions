f81a07399b868a3b8ed8dbff8fac7971d0c4d78a
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-12 01:41:33 +1000

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        size_t left{}, right{}, occupied{}, volume{};
+        while(right < height.size()) {            
+            if (height[left] <= height[right]) {
+                volume += (std::min(height[left],height[right]) * ((right - left) - 1)) - occupied;
+                left = right;
+                occupied = 0;
+            } else {
+                occupied += height[right];
+            }
+            right++;
+        }
+
+        return static_cast<int>(volume);
+    }
+};
