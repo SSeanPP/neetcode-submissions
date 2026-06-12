366aabbf054768c6f9f9dfcb1c132014ad0666e3
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-12 14:47:22 +1000

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::unordered_map<char,char> elements{
+            {'(',')'},
+            {'{','}'},
+            {'[',']'}
+        };
+        std::stack<char> stk{};
+
+        for(char c : s) {
+            if(elements.contains(c)) {
+                stk.push(c);
+            } else {
+                if(stk.empty() || c != elements.at(stk.top())) return false;
+                stk.pop();
+            }
+        }
+
+        return (!stk.empty()) ? true : false;
+
+    }
+};
