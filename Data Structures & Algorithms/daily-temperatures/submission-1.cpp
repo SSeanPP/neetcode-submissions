33aa936885b3e1db9e4f8b162ce68456cc057dfb
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-19 23:27:07 +1000

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        std::stack<size_t> stk{};
+        std::vector<int> result(temperatures.size());
+        
+        for (size_t i{}; i < temperatures.size(); i++) {
+            while(!stk.empty() && temperatures[i] >temperatures[stk.top()]) {
+                result[stk.top()] = i-stk.top();
+                stk.pop();
+            }
+
+            stk.push(i);
+        }
+
+        while(!stk.empty()) {
+            result[stk.top()] = 0;
+            stk.pop();
+        }
+
+        return result;
+    }
+};
