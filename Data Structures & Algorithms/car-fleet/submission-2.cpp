a1dfc5dc9b533398fa0f59b7f07cb275458ac80b
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-22 13:34:46 +1000

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        std::stack<double> carTimes{};
+        size_t noFleets{position.size()};
+        
+        std::vector<std::pair<int,int>> cars;
+        cars.reserve(position.size());
+        for(size_t i{}; i < position.size(); i++) cars.emplace_back(position[i], speed[i]);
+        
+        std::sort(cars.begin(), cars.end(), std::greater<>());
+
+        for(size_t i{}; i < cars.size(); i++) {
+            double carETA{((double)(target - cars[i].first) / cars[i].second)};
+            if(carTimes.empty()) {
+                carTimes.push(carETA);
+            } else {
+                if(carETA <= carTimes.top()) {
+                    noFleets--;
+                } else {
+                    carTimes.push(carETA);
+                }
+            }
+
+        }
+
+        return noFleets;
+    }
+};
