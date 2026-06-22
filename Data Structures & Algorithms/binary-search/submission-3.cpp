bde9bb241be8da1d355e64dc8d214f82a8c13407
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-22 14:16:31 +1000

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        size_t left{}, right{nums.size()-1};
+        size_t index{left + (right - left) / 2};
+
+        while(nums[index] != target) {
+            if(nums[index] < target) {
+                left += (right-left) / 2;
+            } else {
+                right -= (right-left) / 2;
+            }
+            
+            index = left + (right - left) / 2;
+            if((index == 0 && nums[index] != target) || index == nums.size() && nums[index] != target) return -1;
+        }
+
+        return index;
+    }
+};
