973030ea6f7710f7e0125bd2fc075f8831c1bb30
Sean Parkinson <seanparkinson76@hotmail.com>
2026-06-27 16:47:18 +1000

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        std::sort(piles.begin(), piles.end());
+        int total{std::accumulate(piles.begin(), piles.end(), 0)};
+        int k{piles[piles.size() - 1]}, left{}, right{static_cast<int>(piles.size() - 1)}, mid{};
+        size_t totalBites{}, eatingSpeed{};
+
+        while (left <= right) {
+            mid = left + ((right - left)) / 2;
+            eatingSpeed = piles[mid];
+            totalBites = 0;
+            for(size_t i{}; i < piles.size(); i++) {
+                totalBites += (piles[i] + eatingSpeed - 1) / eatingSpeed; 
+            }
+
+            if (totalBites < h) {
+                k = eatingSpeed;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return k;
+    }
+};
